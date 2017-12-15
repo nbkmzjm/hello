6a4e5f000ac28118fcfbf091ad8328f146b03376
nbkmzjm <ngokhanhthien@yahoo.com>
2017-12-15 12:19:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-        res.send('hey !!!!!alowowow')
-})
+
+app.use(express.static('public'))
 app.listen(3000, () => console.log('server running on post 3000'))
